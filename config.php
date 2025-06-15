4861116f4fa7c1cf0447ec743363f28a06032e29
kyrstnxx <162955772+kyrstnxx@users.noreply.github.com>
2025-06-15 23:05:31 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "census";
+
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if($conn->connect_error) {
+    die ("Connection failed." . $conn->connect_error);
+
+
+    
+}
+
+
+
+?>
